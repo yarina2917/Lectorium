c4cfdca39684f74720857b056b9497cd5848a0aa
Yana <yarina297@gmail.com>
2018-10-31 21:39:07 +0200

    Homework 3.2 changed

--- Homework 3/main2.js
+++ Homework 3/main2.js
@@ -2,8 +2,15 @@ function transform (arr) {
     let obj = {};
 
     for (let i = 0; i < arr.length; i++) {
-        obj[i] = arr[i]
+        if (arr[i] in obj) {
+            obj[arr[i]].push(i);
+        } else {
+            obj[arr[i]] = [];
+            obj[arr[i]].push(i);
+        }
     }
 
     return obj
 }
+
+console.log(transform([2, 13, 'str', 2, 5, 17, 29, 5, 0]));
