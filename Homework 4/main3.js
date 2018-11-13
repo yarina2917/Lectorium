81e8e534615a7dc671f499f74a163bb8c193a460
Yana <yarina297@gmail.com>
2018-11-14 00:36:25 +0200

    Changed 4.3

--- Homework 4/main3.js
+++ Homework 4/main3.js
@@ -4,39 +4,39 @@ function coordinates(R, C, r0, c0) {
     let arrCoordinates = [];
     arrCoordinates.push([r0, c0]);
 
-    while (arrCoordinates.length < R * C) {
+    let max = Math.max(r0, R-r0-1, c0, C-c0-1);
+
+    for (let j = 0; j < max; j++) {
         for (let i = count; i > 0; i--) {
             c0++;
-            if (c0 >= 0 && c0 < C && r0 >= 0 && r0 < R) {
-                arrCoordinates.push([r0, c0]);
-            }
+            arrCoordinates.push([r0, c0]);
         }
 
         for (let i = count; i > 0; i--) {
             r0++;
-            if (r0 >= 0 && r0 < R && c0 >= 0 && c0 < C) {
-                arrCoordinates.push([r0, c0]);
-            }
+            arrCoordinates.push([r0, c0]);
         }
 
         for (let i = count + 1; i > 0; i--) {
             c0--;
-            if (c0 >= 0 && c0 < C && r0 >= 0 && r0 < R) {
-                arrCoordinates.push([r0, c0]);
-            }
+            arrCoordinates.push([r0, c0]);
         }
 
         for (let i = count + 1; i > 0; i--) {
             r0--;
-            if (r0 >= 0 && r0 < R && c0 >= 0 && c0 < C) {
-                arrCoordinates.push([r0, c0]);
-            }
+            arrCoordinates.push([r0, c0]);
         }
 
         count += 2;
     }
 
-    return arrCoordinates;
+    let filterCoordinates = arrCoordinates.filter(([r0, c0]) => {
+        if (r0 >= 0 && r0 < R && c0 >= 0 && c0 < C) {
+            return true
+        }
+    });
+
+    return filterCoordinates
 }
 
 console.log(coordinates(5, 6, 1, 4));
